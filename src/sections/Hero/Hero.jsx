f6d383bb6c84ac5982f3b9d22f9006b1dd5d6ec8
Magurin <stolarovigor734@gmail.com>
2025-12-18 18:21:51 +0500

    add components:AuthorCard, HeroCard

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import HeroCard from '@/components/HeroCard'
 import './Hero.scss'
 
 export default () => {
@@ -14,9 +15,34 @@ export default () => {
       >
         <ul className="hero__slider-list swiper-wrapper">
           <li className="hero__slider-item swiper-slide">
-            <div className="hero__card">
-              1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, enim beatae cupiditate exercitationem animi aspernatur laboriosam vel eos veritatis neque saepe deleniti eligendi quae earum, at aliquid ut culpa! Amet.
-            </div>
+            <HeroCard  
+              category={{
+                imgSrc: '/src/assets/images/recipe.jpg',
+                label: 'Hot Recipes',
+              }}
+              title="Spicy delicious chicken wings"
+              TitleTag="h1"
+              description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+              tags={[
+                {
+                  iconName: 'timer',
+                  label: '30 Minutes',
+                },
+                {
+                  iconName: 'fork-knife',
+                  label: 'Chicken',
+                },
+              ]}
+              author={{
+                imgSrc: '/src/assets/images/man.jpg',
+                name: 'John Smith',
+                date: {
+                  dateTime: '2022-03-15',
+                  label: '15 March 2022',
+                }
+              }}
+              imgSrc="/src/assets/images/chicken.jpg"
+            />
           </li>
           <li className="hero__slider-item swiper-slide">
             <div className="hero__card">
